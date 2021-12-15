abe3bcd021bccfea4b0c36d2c8a9a8f1d3eee7a6
MaisamCoder <77421059+MaisamCoder@users.noreply.github.com>
2021-12-15 18:37:08 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent("game_console");
+	posenet = ml5.poseNet('posenet', modelLoaded);
+	posenet.on('pose', gotPoses);
+}
+
+function draw() {
+	game()
+}
+
+function gotPoses(results)
+{
+	if(results.length > 0)
+	{
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+	}
+}
+
+function modelLoaded()
+{
+	console.log("Model Loaded!")
+}
+
+
+
+
+
+
